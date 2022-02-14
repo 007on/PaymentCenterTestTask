0adf45cfdd9aed5f1fffb51f6e7de1a4bfce4c40
S K <semmmkp@gmail.com>
2022-02-15 02:02:24 +0300

    add manager

---
+++ PaymentCenterTestTask/Managers/BroPayManager.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using PaymentCenterTestTask.Clients;
+using PaymentCenterTestTask.Data.BroPayApi;
+using System.Threading.Tasks;
+
+namespace PaymentCenterTestTask.Managers {
+    public class BroPayManager : IBroPayManager {
+        private readonly string _payMethodName = "/pay";
+        private readonly string _payoutMethodName = "/payout";
+        private readonly IBroPayApiClient _broPayApiClient;
+
+        public BroPayManager(IBroPayApiClient broPayApiClient) {
+            _broPayApiClient = broPayApiClient;
+        }
+
+        public async Task<PayResponse> SendPayRequest(PayRequest payRequest) {
+            var jsonResult = await _broPayApiClient.SendRequest(_payMethodName, payRequest);
+
+            return JsonConvert.DeserializeObject<PayResponse>(jsonResult);
+        }
+
+        public async Task<PayoutResponse> SendPayoutRequest(PayoutRequest payoutRequest) {
+            var jsonResult = await _broPayApiClient.SendRequest(_payoutMethodName, payoutRequest);
+
+            return JsonConvert.DeserializeObject<PayoutResponse>(jsonResult);
+        }
+    }
+}
---
+++ PaymentCenterTestTask/Managers/IBroPayManager.cs
@@ -0,0 +1,9 @@
+﻿using PaymentCenterTestTask.Data.BroPayApi;
+using System.Threading.Tasks;
+
+namespace PaymentCenterTestTask.Managers {
+    public interface IBroPayManager {
+        Task<PayoutResponse> SendPayoutRequest(PayoutRequest payoutRequest);
+        Task<PayResponse> SendPayRequest(PayRequest payRequest);
+    }
+}
