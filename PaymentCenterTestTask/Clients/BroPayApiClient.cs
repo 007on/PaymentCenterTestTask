bd4ea3beece811d12a51f3301a197f4ad5bf30c8
S K <semmmkp@gmail.com>
2022-02-15 02:02:07 +0300

    add broPay client

---
+++ PaymentCenterTestTask/Clients/BroPayApiClient.cs
@@ -0,0 +1,28 @@
+﻿using PaymentCenterTestTask.Configs;
+using PaymentCenterTestTask.Data.BroPayApi;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentCenterTestTask.Clients {
+    public class BroPayApiClient : IBroPayApiClient {
+        private readonly HttpClient _httpClient;
+
+        public BroPayApiClient(BroPayConfig config) {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(config.Url);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> SendRequest(string method, PayRequestBase request) {
+            var response = await _httpClient.PostAsJsonAsync(method, request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ PaymentCenterTestTask/Clients/IBroPayApiClient.cs
@@ -0,0 +1,8 @@
+﻿using PaymentCenterTestTask.Data.BroPayApi;
+using System.Threading.Tasks;
+
+namespace PaymentCenterTestTask.Clients {
+    public interface IBroPayApiClient {
+        Task<string> SendRequest(string method, PayRequestBase request);
+    }
+}
