4d0abe4d25731eeb91a983d06ea339c527f49ac6
S K <semmmkp@gmail.com>
2022-02-15 02:02:53 +0300

    add class of config

---
+++ PaymentCenterTestTask/Configs/BroPayConfig.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace PaymentCenterTestTask.Configs {
+    public class BroPayConfig {
+        public BroPayConfig() {
+            var json = File.ReadAllText("BroPayUrls");
+            var config = JsonConvert.DeserializeAnonymousType(
+                json,
+                new { TestMode = false, TestUrl = "", ProdUrl = "" }
+            );
+            Url = config.TestMode ? config.TestUrl : config.ProdUrl;
+        }
+
+        public string Url { get; }
+    }
+}
